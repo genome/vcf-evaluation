64c411290b6f27290e977ff285ebba7a4ccfc24d
Dave Larson <dlarson@genome.wustl.edu>
2014-05-05 23:25:57 -0500

    beginning of wrapper to evaluate VCFs in a modelgroup

---
+++ evaluate_modelgroup.pl
@@ -0,0 +1,71 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use lib "/gscmnt/gc3042/cle_validation/src/genome/lib/perl/";
+
+use File::Spec;
+use Genome;
+use Cwd;
+
+#inputs: reference, tn bed, indel variant vcf, snv variant vcf, model group, roi
+my $model_group;
+my $roi;
+my $gold_snv_vcf;
+my $gold_indel_vcf;
+my $tn_bed;
+my $help;
+
+GetOptions(
+    'model-group=s' => \$model_group,
+    'roi=s' => \$roi,
+    'gold-snv-vcf=s' => \$gold_snv_vcf,
+    'gold-indel-vcf=s' => \$gold_indel_vcf,
+    'true-negative-bed=s' => \$tn_bed,
+    'help!' => \$help,
+) or print_help();
+print_help() if $help;
+
+for my $build (Genome::ModelGroup->get($model_group)->builds) {
+    
+    my $dirname = $build->id;
+    unless(-d $dirname) {
+        mkdir $dirname;
+    }
+
+    for my $variant_type qw( snvs indels ) {
+        my $cwd = cwd();
+        my $dirname = $build->id . "/$variant_type";
+        unless(-d $dirname) {
+            mkdir $dirname;
+        }
+        chdir $dirname;
+        my $file = $build->id->data_directory . "/variants/$variant_type.vcf.gz";
+        my $file_name = filename($file);
+        symlink $file, $file_name;
+        my $roi_name = filename($roi);
+        symlink $roi, $roi_name;
+        my $tn_bed_name = filename($tn_bed);
+        symlink $tn_bed, $tn_bed_name;
+        my $gold_file = $variant_type eq "snvs" ? $gold_snv_vcf : $gold_indel_vcf;
+        my $gold_file_name = filename($gold_file);
+        symlink $gold_file, $gold_file_name;
+        my @output = `perl /gscmnt/gc3042/cle_validation/src/evaluation/evaluate_vcf.pl --vcf $file_name --roi $roi_name --gold-vcf $gold_file_name --true-negative-bed $tn_bed_name`;
+        print $build->model->name, "\t", $output[0];
+        chdir $cwd;
+    }
+
+}
+
+sub filename {
+    my $path = shift;
+    my @stuff = File::Spec->splitpath( $path );
+    return $stuff[2];
+}
+
+sub print_help {
+    print STDERR "evaluate_modelgroup --model-group --roi --gold-snv-vcf --gold-indel-vcf --true-negative-bed\n";
+    exit;
+}
+
